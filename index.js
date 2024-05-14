9b33a19dc2dcbcb7db201e9e288f3eba879ef9b1
Uppnoor <uppnoor41@gmail.com>
2024-05-14 13:09:48 -0700

    Set up the template workplace for the project. The template for the project is an node/ejs application. The mongodb is connected and the application works on port:8000 .Make sure to download the .env file aswell as the dotevn dependency as it will not be uploaded in the commit.

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+const Joi = require('joi');
+const saltRounds = 12;
+const MongoStore = require('connect-mongo');
+const mongoose = require('mongoose');
+
+const port = 8000;
+
+// MongoDB URI
+const mongoUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB COMP2800');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.static('.'));
+app.use(express.urlencoded({ extended: true }));
+
+// Simple route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+
