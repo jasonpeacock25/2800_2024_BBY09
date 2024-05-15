88d412f17d3e02ad0ed90cf6e426ad103429850c
Gurvir Dhami <gurvirsd@gmail.com>
2024-05-15 15:51:50 -0700

    Added The hotel dates entry page, available hotels page, and hotel summary page.The functionaliy is surface level with placeholder content and minimum interaction with the database.

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const mongoose = require('mongoose');
 
 const port = 8000;
 
+// Import the Hotel model
+const Hotel = require('./models/Hotel');
+
 // MongoDB URI
 const mongoUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`;
 
@@ -28,12 +31,52 @@ mongoose.connect(mongoUri, {
 app.set('view engine', 'ejs');
 
 // Middleware
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('.'));
 app.use(express.urlencoded({ extended: true }));
 
 // Simple route
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+
+// Gurvir's Routes
+app.get('/hotels', (req, res) => {
+    res.render('hotels');
+});
+
+app.post('/search', async (req, res) => {
+    const region = req.body.region;
+    const checkInDate = req.body.checkIn;
+    const checkOutDate = req.body.checkOut;
+
+    req.session.hotelCheckInDate = checkInDate;
+    req.session.hotelCheckOutDate = checkOutDate;
+
+    // const hotels = await Hotel.find();
+    // res.render('availableHotels', { hotels }, region);
+
+});
+
+app.get('/availableHotels', async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+        res.render('availableHotels', { hotels });
+    } catch (err) {
+        console.error('Error fetching hotels:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/hotelSelection', async (req, res) => {
+    const hotelID = req.body.hotelId;
+    res.redirect(`hotelSummary/${hotelID}`);
+});
+
+app.get('/hotelSummary/:id', async (req, res) => {
+    const hotel = await Hotel.findById(req.params.id);
+    res.render('hotelSummary', { hotel });
 });
 
 app.get('/about', (req, res) => {
